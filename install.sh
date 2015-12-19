c6188972da6ff23a91138fa6e84523f802c1cebe
chai <861503125@qq.com>
2015-12-19 15:42:22 +0800

    first commit

---
+++ install.sh
@@ -0,0 +1,31 @@
+sudo apt-get update
+sudo apt-get install openssh-server
+
+# install remote desktop
+sudo apt-get install xrdp
+sudo apt-get install vnc4server
+sudo apt-get install xubuntu-desktop
+echo "xfce4-session" >~/.xsession
+sudo service xrdp restart
+
+sudo apt-get install vim
+sudo apt-get install git
+sudo apt-get install python-pip
+
+# k-vim
+sudo apt-get install ctags
+sudo apt-get install build-essential cmake python-dev  #编译YCM自动补全插件依赖
+sudo apt-get install silversearcher-ag
+
+sudo pip install pyflakes
+sudo pip install pylint
+sudo pip install pep8
+
+sudo apt-get install nodejs npm
+sudo npm install -g jslint
+sudo npm install jshint -g
+
+git clone git@github.com:wklken/k-vim.git
+cd k-vim && sh -x install.sh
+sudo apt-get install redis-server
+sudo pip install redis
